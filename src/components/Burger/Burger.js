c4df9139ace93b07b3008e42c12fde04543f288a
nemanjarogic <nemanjarogic93@gmail.com>
2019-12-09 23:10:28 +0100

    Outputting burger ingredients dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,25 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 import classes from "./Burger.module.css";
 
 const Burger = props => {
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((_, index) => {
+        return <BurgerIngredient key={igKey + index} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      //flatter the array
+      return arr.concat(el);
+    }, []);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -19,16 +19,16 @@ class BurgerIngredient extends Component {
         );
         break;
       case "meat":
-        ingredient = <div className={classes.BreadBottom}></div>;
+        ingredient = <div className={classes.Meat}></div>;
         break;
       case "cheese":
-        ingredient = <div className={classes.BreadBottom}></div>;
+        ingredient = <div className={classes.Cheese}></div>;
         break;
       case "bacon":
-        ingredient = <div className={classes.BreadBottom}></div>;
+        ingredient = <div className={classes.Bacon}></div>;
         break;
       case "salad":
-        ingredient = <div className={classes.BreadBottom}></div>;
+        ingredient = <div className={classes.Salad}></div>;
         break;
       default:
         ingredient = null;

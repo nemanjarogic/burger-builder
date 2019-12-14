bcef9e31561c9b0b63a803da6708344f67b7fcef
nemanjarogic <nemanjarogic93@gmail.com>
2019-12-14 08:20:38 +0100

    Changing folder structure

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from "react";
-import Toolbar from "../Navigation/Toolbar/Toolbar";
-import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
+import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
+import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
 import styles from "./Layout.module.css";
 
 class Layout extends Component {

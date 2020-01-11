84b1e0738fd45b33dfb895d6c8dbbd46f068d482
nemanjarogic <nemanjarogic93@gmail.com>
2020-01-11 09:12:05 +0100

    Basic component, container and redux state testing

---
+++ src/store/reducers/authentication.test.js
@@ -0,0 +1,35 @@
+import reducer from "./authentication";
+import * as actionTypes from "../actions/actionTypes";
+
+describe("Authentication reducer", () => {
+  let initialState;
+  beforeAll(() => {
+    initialState = {
+      token: null,
+      userId: null,
+      error: null,
+      isLoading: false,
+      authRedirectPath: "/"
+    };
+  });
+
+  it("should return the initial state", () => {
+    expect(reducer(undefined, {})).toEqual(initialState);
+  });
+
+  it("should store user token upon login", () => {
+    const successLoginPayload = {
+      type: actionTypes.AUTHENTICATION_SUCCEEDED,
+      token: "test-token",
+      userId: "test-user-id"
+    };
+
+    expect(reducer(initialState, successLoginPayload)).toEqual({
+      token: "test-token",
+      userId: "test-user-id",
+      error: null,
+      isLoading: false,
+      authRedirectPath: "/"
+    });
+  });
+});
